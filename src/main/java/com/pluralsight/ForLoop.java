5f6da5e9d95772cb48e5067f43ffda5fe4f81b83
lewiskangethe <lewiskkangethe@gmail.com>
2025-10-02 16:44:20 -0400

    updated commit

--- src/main/java/com/pluralsight/ForLoop.java
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -1,10 +1,11 @@
 package com.pluralsight;
 
 public class ForLoop {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         for (int countdown = 10; countdown >=1; countdown--){
             System.out.println(countdown);
         }
         System.out.println("Launch!");
+        Thread.sleep(1000);
     }
 }
