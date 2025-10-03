d4342ba6187433131b013a7d36507e819f2f2940
lewiskangethe <lewiskkangethe@gmail.com>
2025-10-03 13:48:29 -0400

    arrays initial commit

---
+++ src/main/java/com/pluralsight/ArrayExample.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        int numbers[] = {55,66,87,23,9,86};
+
+        int numberA = numbers[3];
+        System.out.println(numberA);
+        System.out.println(numbers[2]);
+
+
+        String students[] = {"Lucas", "Marianna", "Martha", "Nam", "Vincent", "Romell"};
+
+        String studentA = students[3];
+        System.out.println(studentA);
+        System.out.println(students[1]);
+    }
+
+}
